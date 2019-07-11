c3591f9a2896387c5ce21efdd6820f4827e65f6b
TheOne <tanwei.rush@gmail.com>
2019-07-11 15:25:12 +0800

    1. init 2. 3. 4.

---
+++ README.md
@@ -0,0 +1,9 @@
+# 提供了jemalloc和cgomalloc两种方式的 make []byte 
+
+## jemalloc 安装
+
+```bash
+go get -v github.com/spinlock/jemalloc-go
+cd $GOPATH/src/github.com/spinlock/jemalloc-go
+make install
+```
---
+++ assert/assert.go
@@ -0,0 +1,22 @@
+// Copyright 2016 CodisLabs. All Rights Reserved.
+// Licensed under the MIT (MIT-LICENSE.txt) license.
+
+package assert
+
+import (
+	"log"
+)
+
+func Must(b bool) {
+	if b {
+		return
+	}
+	log.Panic("assertion failed")
+}
+
+func MustNoError(err error) {
+	if err == nil {
+		return
+	}
+	log.Panic(err, "error happens, assertion failed")
+}
